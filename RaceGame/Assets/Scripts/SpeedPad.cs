fb73d4aaa81eb102a4d75a2bc081018a22adfecd
Ilian Petrov <ilianpetrov.personal@gmail.com>
2025-09-10 15:28:16 +0200

    Speed Pad + Jump Pad

---
+++ RaceGame/Assets/Scripts/SpeedPad.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SpeedPad : MonoBehaviour
+{
+    public enum SpeedDirection { Forward, Backwards }
+
+    public SpeedDirection speedDirection;
+
+    public float boostStrength = 20f;
+    public float boostDuration = 1f; //large number = more time to get to the destination
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            VelocityChanger boost = rb.GetComponent<VelocityChanger>();
+
+            if (boost != null)
+            {
+                if (speedDirection == SpeedDirection.Forward)
+                {
+                    boost.ApplyBoost(transform.forward, boostStrength, boostDuration); //the calculation of the boost
+                }
+
+                else if (speedDirection == SpeedDirection.Backwards)
+                {
+                    boost.ApplyBoost(-transform.forward, boostStrength, boostDuration); //the calculation of the boost
+                }
+            }
+        }
+    }
+}
