44576db021d313aefff45edc16bd1d7eae6cb86a
PCx200 <petko05raychinov@gmail.com>
2025-09-10 14:21:53 +0300

    Working cars

--- RaceGame/Assets/Scripts/Movement.cs
+++ RaceGame/Assets/Scripts/Movement.cs
@@ -4,41 +4,45 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed;
+    [Header("Car Settings")]
+    [SerializeField] float motorForce = 100f;
+    [SerializeField] float breakForce = 1000f;
+    [SerializeField] float maxSteerAngle = 30f;
+
+    [Header("Wheels")]
+    [SerializeField] WheelCollider frontLeftWheelCollider;
+    [SerializeField] WheelCollider frontRightWheelCollider;
+    [SerializeField] WheelCollider backLeftWheelCollider;
+    [SerializeField] WheelCollider backRightWheelCollider;
+
+    [SerializeField] Transform frontLeftWheelTransform;
+    [SerializeField] Transform frontRightWheelTransform;
+    [SerializeField] Transform backLeftWheelTransform;
+    [SerializeField] Transform backRightWheelTransform;
+
+    //[SerializeField] float movementSpeed;
     Vector2 movementInput;
 
     bool isBraking;
-    [SerializeField] float brakingPower;
+
+    private float currentSteerAngle;
+    private float currentBreakForce;
+
+    //[SerializeField] float brakingPower;
 
     void Update()
     {
         if (GameManager.Instance.isSplited)
-        { 
-            Move();
+        {
+            HandleMotor();
+            HandleSteering();
+            UpdateWheels();
         }
     }
 
     public void OnMove(InputValue input)
     {
         movementInput = input.Get<Vector2>();
-    }
-
-    private void Move()
-    {
-        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-
-        float currentSpeed = movementSpeed;
-        if (isBraking)
-        {
-            currentSpeed *= brakingPower;
-        }
-        else
-        {
-            currentSpeed = movementSpeed;
-        }
-
-
-        transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
     }
     public void OnBreak(InputValue input)
     {
@@ -50,4 +54,65 @@ public class Movement : MonoBehaviour
         }
         else { Debug.Log("Not Breaking"); }
     }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = movementInput.y * motorForce;
+        frontRightWheelCollider.motorTorque = movementInput.y * motorForce;
+
+        currentBreakForce = isBraking ? breakForce : 0f;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        float brake = isBraking ? currentBreakForce : 0f;
+
+        frontLeftWheelCollider.brakeTorque = brake;
+        frontRightWheelCollider.brakeTorque = brake;
+        backLeftWheelCollider.brakeTorque = brake;
+        backRightWheelCollider.brakeTorque = brake;
+       
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * movementInput.x;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelColider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelColider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(backLeftWheelCollider, backLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(backRightWheelCollider, backRightWheelTransform);
+    }
+    //private void Move()
+    //{
+    //    Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+
+    //    float currentSpeed = movementSpeed;
+    //    if (isBraking)
+    //    {
+    //        currentSpeed *= brakingPower;
+    //    }
+    //    else
+    //    {
+    //        currentSpeed = movementSpeed;
+    //    }
+
+
+    //    transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
+    //}
 }
